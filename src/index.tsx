56d83a82f9214f2be89302ac70aa3415f7358987
konojunya <junya03dance@gmail.com>
2024-09-24 08:02:49 +0900

    feat: src

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client';
+import {Routes} from '@generouted/react-router';
+
+// eslint-disable-next-line unicorn/prefer-query-selector
+ReactDOM.createRoot(document.getElementById('root')!).render(<Routes />);
